3d7a2e6895f22d84b77502adcb37b3b34fbf87a8
loongfee <loongfee@gmail.com>
2015-09-07 16:56:54 +0800

    updated site 20150907

--- upload.py
+++ upload.py
@@ -13,6 +13,8 @@ import stat
             
 
 
+# 用户名
+usr = 'loongfee'
 # 获取日期：
 today =datetime.date.today()    #获取今天日期
 # 格式化输出
@@ -20,8 +22,12 @@ ISOFORMAT='%Y%m%d' #设置输出格式
 date = today.strftime(ISOFORMAT)
 
 comment = date
+os.system('git init')
 os.system('git add .')
 os.system('git commit -m "updated site %s"' %comment)
-os.system('git push origin source')
+os.system(r'git config remote.origin.url https://%s:a9595965@github.com/%s/%s.github.com.git'%(usr,usr,usr))
+os.system(r'git remote add origin https://github.com/%s/%s.github.com.git'%(usr,usr))
+os.system(r'git remote set-url origin https://github.com/%s/%s.github.com.git'%(usr,usr))
+os.system('git push origin source -f')
 
 os.system('pause')
