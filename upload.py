02e1c14d498fc910518f4bbf24800cb630354857
loongfee <loongfee@gmail.com>
2015-09-09 16:06:47 +0800

    updated site 20150909

--- upload.py
+++ upload.py
@@ -25,7 +25,6 @@ comment = date
 os.system('git init')
 os.system('git add .')
 os.system('git commit -m "updated site %s"' %comment)
-#os.system(r'git config remote.origin.url https://%s:a9595965@github.com/%s/%s.github.com.git'%(usr,usr,usr))
 os.system('git config --global credential.helper wincred')
 os.system(r'git remote add origin https://github.com/%s/%s.github.com.git'%(usr,usr))
 os.system(r'git remote set-url origin https://github.com/%s/%s.github.com.git'%(usr,usr))
