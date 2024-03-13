0c4e67ada2899d4bd3e0907932d6d6b7acf9608d
loongfee <loongfee@126.com>
2024-03-14 00:25:10 +0800

    updated site 20240314

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-SET PortableJekyll=D:\opensource\PortableJekyll
+SET PortableJekyll=Z:\longtf\opensource\PortableJekyll
 @set http_proxy=http://127.0.0.1:10809
 @set https_proxy=http://127.0.0.1:10809
 SET PATH=%PortableJekyll%\ruby\bin;%PortableJekyll%\devkit\bin;%PortableJekyll%\git\bin;%PortableJekyll%\Python\App;%PortableJekyll%\Python\App\Scripts;%PortableJekyll%\devkit\mingw\bin;%PortableJekyll%\curl\bin;%PATH%;
