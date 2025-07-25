8d4da5a9493603ec4bbe4742c591d5f89b70b773
loongfee <vapd.radi@gmail.com>
2025-07-25 15:40:32 +0800

    updated site 20250725

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 SET PortableJekyll=Z:\longtf\opensource\PortableJekyll
-@set http_proxy=http://192.168.66.126:7897
-@set https_proxy=http://192.168.66.126:7897
+@set http_proxy=http://192.168.66.126:7890
+@set https_proxy=http://192.168.66.126:7890
 SET PATH=%PortableJekyll%\ruby\bin;%PortableJekyll%\devkit\bin;%PortableJekyll%\git\bin;%PortableJekyll%\Python\App;%PortableJekyll%\Python\App\Scripts;%PortableJekyll%\devkit\mingw\bin;%PortableJekyll%\curl\bin;%PATH%;
 
 set SSL_CERT_FILE=%PortableJekyll%\curl\bin\cacert.pem
