74b0d90184df0ca27cbe5becc92aa937bf200080
loongfee <vapd.radi@gmail.com>
2025-09-16 22:23:53 +0800

    updated site 20250916

--- run.bat
+++ run.bat
@@ -8,8 +8,8 @@ REM Optional proxies (comment out if not needed)
 SET "http_proxy=http://192.168.66.105:7890"
 SET "https_proxy=http://192.168.66.105:7890"
 
-REM Build a minimal PATH required by PortableJekyll (do not append existing PATH)
-SET "PATH=%PortableJekyll%\ruby\bin;%PortableJekyll%\devkit\bin;%PortableJekyll%\git\bin;%PortableJekyll%\Python\App;%PortableJekyll%\Python\App\Scripts;%PortableJekyll%\devkit\mingw\bin;%PortableJekyll%\curl\bin"
+REM Build a minimal PATH required by PortableJekyll, plus System32 for built-ins like chcp
+SET "PATH=%SystemRoot%\System32;%PortableJekyll%\ruby\bin;%PortableJekyll%\devkit\bin;%PortableJekyll%\git\bin;%PortableJekyll%\Python\App;%PortableJekyll%\Python\App\Scripts;%PortableJekyll%\devkit\mingw\bin;%PortableJekyll%\curl\bin"
 SET "SSL_CERT_FILE=%PortableJekyll%\curl\bin\cacert.pem"
 
 IF NOT EXIST "%PortableJekyll%" (
